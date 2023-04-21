c359f2ebfa469f81b18c747fbc8f825ad4f94b7b
Henry Finucane <hfinucane@zscaler.com>
2023-04-21 20:41:07 +0000

    Guess at where the build scripts should be modified

--- build_scripts/build_centos_platforms.sh
+++ build_scripts/build_centos_platforms.sh
@@ -10,3 +10,10 @@ retval=$?
 if [[ retval -ne 0 ]]; then
   exit $retval
 fi
+
+# EL 9 platform
+platform="el9" test_platforms="almalinux:9 quay.io/centos/centos:stream9 rockylinux:9 registry.access.redhat.com/ubi9/ubi:latest" ./build_and_test_platform.sh
+retval=$?
+if [[ retval -ne 0 ]]; then
+  exit $retval
+fi
--- build_scripts/build_platforms.sh
+++ build_scripts/build_platforms.sh
@@ -50,6 +50,13 @@ if [[ retval -ne 0 ]]; then
   exit $retval
 fi
 
+# EL 9 platform
+platform="el9" test_platforms="almalinux:9 quay.io/centos/centos:stream9 rockylinux:9 registry.access.redhat.com/ubi9/ubi:latest" ./build_and_test_platform.sh
+retval=$?
+if [[ retval -ne 0 ]]; then
+  exit $retval
+fi
+
 # CentOS 6 platform
 platform="centos6" test_platforms="centos:6 centos:7 debian:8 debian:9 debian:10 ubuntu:14.04 ubuntu:16.04 ubuntu:18.04" ./build_and_test_platform.sh
 retval=$?
